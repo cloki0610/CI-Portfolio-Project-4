a06df9227a3f4282b11c9de2a113cc317f3d89f1
Rocky.C <cloki0610@gmail.com>
2021-11-30 23:18:19 +0000

    add a simple nav bar and complete allauth setting

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
+"""
+HOME PAGE APP VIEWS
+"""
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    """ simple view of the index.html """
+    return render(request, 'home/index.html')
