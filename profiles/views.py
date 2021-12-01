f992ce36ee409ee89bacfca183d0f3736cc2ab51
Rocky.C <cloki0610@gmail.com>
2021-12-01 23:52:29 +0000

    complete profile with admin function

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views import generic, View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import UserProfile
 
-# Create your views here.
+
+class UserProfileView(LoginRequiredMixin, generic.DetailView):
+    """ A view to return the data required user profile"""
+    model = User
+    template_name = 'profiles/profile.html'
+
+    def get_object(self):
+        pass
