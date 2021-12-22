b329c39e07322313cac286dbe166221afe352a77
Rocky.C <cloki0610@gmail.com>
2021-12-22 16:36:42 +0000

    complete home app style

--- home/views.py
+++ home/views.py
@@ -13,12 +13,15 @@ class HomePage(View):
     """ List all the theme from latest to oldest """
     def get(self, request):
         """ GET method """
+        # get the list of required data
         all_theme = Theme.objects.all()
         top_theme = Theme.objects.annotate(most_upvote=Count('upvote')) \
                          .order_by('-most_upvote')[:3]
+        # use paginator to manage the result
         theme_paginator = Paginator(all_theme, 10)
         page_number = request.GET.get('page')
         theme_page = theme_paginator.get_page(page_number)
+        # return data with template
         return render(
             request,
             "home/index.html",
@@ -33,11 +36,14 @@ class CategoryView(View):
     """ List all the theme in the category """
     def get(self, request, category_slug):
         """ GET method """
+        # get the list of required data
         category = get_object_or_404(Category, slug=category_slug)
         themes = category.category_theme.order_by('-updated_on')
+        # use paginator to manage the result
         theme_paginator = Paginator(themes, 10)
         page_number = request.GET.get('page')
         theme_page = theme_paginator.get_page(page_number)
+        # return data with template
         return render(
             request,
             "home/category.html",
